f001cad7762935d9f0160ca2f97882e65a906ce9
Aswin N S <naswin186@gmail.com>
2024-05-05 22:39:58 +0530

    3rd

--- hcr/hcr/settings.py
+++ hcr/hcr/settings.py
@@ -119,10 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 # STATIC_ROOT = BASE_DIR / 'productionfiles'
-STATIC_URL = '/static/' #ishtam ulla url name kodukkaaa
-STATICFILES_DIRS = [
-    BASE_DIR/ "static",
-]
+STATIC_URL = 'static/' #ishtam ulla url name kodukkaaa
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
